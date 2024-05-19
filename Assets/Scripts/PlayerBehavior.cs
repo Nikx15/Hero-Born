5f3c5063eea2bb3edfa865f61a61aba24cedf93b
Nikx <nikoagard131524@gmail.com>
2024-05-18 22:34:26 -0400

    Added Jump

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,19 +6,22 @@ public class PlayerBehavior : MonoBehaviour
 {
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
-    private float _vInput;
-    private float _hInput;
-    private Rigidbody _rb;
+    private float VInput;
+    private float HInput;
+    private Rigidbody rb;
+    public float JumpVelocity = 5f;
+    private bool Jumping;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        _vInput = Input.GetAxis("Vertical") * MoveSpeed;
-        _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+        Jumping |= Input.GetKeyDown(KeyCode.Space);
+        VInput = Input.GetAxis("Vertical") * MoveSpeed;
+        HInput = Input.GetAxis("Horizontal") * RotateSpeed;
         /*this.transform.Translate(Vector3.forward * _vInput *
         Time.deltaTime);
         this.transform.Rotate(Vector3.up * _hInput *
@@ -27,11 +30,16 @@ public class PlayerBehavior : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 rotation = Vector3.up * _hInput;
+        if (Jumping)
+        {
+            rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        Jumping = false;
+        Vector3 rotation = Vector3.up * HInput;
         Quaternion angleRot = Quaternion.Euler(rotation *
         Time.fixedDeltaTime);
-        _rb.MovePosition(this.transform.position +
-        this.transform.forward * _vInput * Time.fixedDeltaTime);
-        _rb.MoveRotation(_rb.rotation * angleRot);
+        rb.MovePosition(this.transform.position +
+        this.transform.forward * VInput * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * angleRot);
     }
 }
